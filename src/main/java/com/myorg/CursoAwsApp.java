95efa158d84ac0d91e44f449a738c7a678860b6b
Marcelo <marcelojunin2010@hotmail.com>
2021-03-21 09:35:47 -0300

    building vpc
    command: cdk deploy [VPC_NAME]

--- src/main/java/com/myorg/CursoAwsApp.java
+++ src/main/java/com/myorg/CursoAwsApp.java
@@ -5,11 +5,13 @@ import software.amazon.awscdk.core.App;
 import java.util.Arrays;
 
 public class CursoAwsApp {
+
+    private static final String VPC_NAME = "Vpc";
+
     public static void main(final String[] args) {
         App app = new App();
 
-        new CursoAwsStack(app, "CursoAwsStack");
-
+        new VpcStack(app, VPC_NAME);
         app.synth();
     }
 }
